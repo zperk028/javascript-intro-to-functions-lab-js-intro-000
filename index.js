d3454b61e341bbd17d9961a6dbb135588cb8f990
Zachary Perkins <ezmac028@gmail.com>
2020-10-02 02:11:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {return "I can't hear you!"}
-  else if (string === string.toUpperCase) {return "YES INDEED!"}
+  if (string === string.toLowerCase()) {return "I can't hear you!"}
+  else if (string === string.toUpperCase()) {return "YES INDEED!"}
   else if (string === "I love you, Grandma") {return "I love you, too"}
 }
